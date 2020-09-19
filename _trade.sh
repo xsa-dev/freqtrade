9ee9c985166211849bc69cc115efb1ac656fdb1d
Aleksey SAvin <saleksey67@gmail.com>
2020-09-19 16:11:34 +0300

    search for best results

--- _trade.sh
+++ _trade.sh
@@ -1,11 +1,18 @@
-freqtrade trade --test-pairlist -s BbandRsi -c user_data/config.json --logfile user_data/logs/BTC_BbandRsi.log
+STRATEGY=BBRSI
+rm user_data/logs/$STRATEGY.log
+freqtrade trade --strategy $STRATEGY --config user_data/config.json --logfile user_data/logs/$STRATEGY.log --db-url sqlite:///user_data/tradesv3.dry_run.sqlite
 
 
-:'
+:"
 ``` bash
 # Terminal 1:
 freqtrade trade -c MyConfigBTC.json -s MyCustomStrategy --db-url sqlite:///user_data/tradesBTC.dryrun.sqlite
 # Terminal 2:
 freqtrade trade -c MyConfigUSDT.json -s MyCustomStrategy --db-url sqlite:///user_data/tradesUSDT.dryrun.sqlite
 ```
-'
+"
+
+
+:"
+--test-pairlist
+"
