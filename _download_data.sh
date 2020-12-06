d063b43c3b5ec0a9f07df6f425a09eb3b24fd568
Aleksey Savin <saleksey67@gmail.com>
2020-12-06 09:54:34 +0300

    new strategie tests

--- _download_data.sh
+++ _download_data.sh
@@ -1,2 +1,5 @@
-freqtrade download-data -p {"ETH/BTC","LTC/BTC"} -t {5m,15m,1h} --days 1000
-#freqtrade download-data -p {"ETH/BTC","LTC/BTC"} -t {1m,3m,5m,15m,30m,1h,2h,4h,6h,8h,12h,1d,3d,1w} --erase --days 1000
+# freqtrade download-data -t
+# freqtrade download-data -p {"ETH/BTC","LTC/BTC"} -t {1m,3m,5m,15m,30m,1h,2h,4h,6h,8h,12h,1d,3d,1w} --erase --days 1000
+freqtrade download-data --pairs-file pairs.json -t {3m,5m,15m,30m,1h} --days 100
+
+#freqtrade download-data --data_dir user_data --exchange binance
--- _hyperbacktest.sh
+++ _hyperbacktest.sh
@@ -1,2 +1,3 @@
 # i need to test algo on 10 range of data and calc avg plan profit
-freqtrade backtesting -s BbandRsi -c user_data/config_backtest.json
+#
+freqtrade backtesting -s SuperTrend -c user_data/config_backtest.json
